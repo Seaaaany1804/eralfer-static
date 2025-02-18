4a54385187618de78630d51705e8675064c18593
sean <sean.dexter@zktech.solutions>
2025-02-18 10:33:16 +0800

    fix: remove user api

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,27 +1,20 @@
-// /* eslint-disable @typescript-eslint/no-unused-vars */
 // import { NextRequest, NextResponse } from "next/server";
+// import { handleApiError } from "@/app/utils/errorHandler";
 // import { UserService } from "@/services/userService";
 
-// export async function GET(
-//   request: NextRequest,
-//   { params }: { params: { id: string } }
-// ) {
+// // 📌 GET: Fetch a single user by ID
+// export async function GET(req: NextRequest, { params }: { params: { id: number } }) {
 //   try {
-//     const userId = Number(params.id);
-
-//     if (isNaN(userId)) {
-//       return NextResponse.json({ error: "Invalid user ID" }, { status: 400 });
-//     }
-
+//     const userId = params.id;
+//     // Fetch user from DB
 //     const user = await UserService.getUserById(userId);
-
+    
 //     if (!user) {
 //       return NextResponse.json({ error: "User not found" }, { status: 404 });
 //     }
-
 //     return NextResponse.json(user, { status: 200 });
 
 //   } catch (error) {
-//     return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+//     return handleApiError({ error: "Error fetching user by ID: " + error });    
 //   }
 // }
